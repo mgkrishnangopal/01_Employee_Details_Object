3c0b6b6d6627770da0bcb60ecada6450edfd7c04
GOPALAKRISHNAN MURUGAN <135576063+mgkrishnangopal@users.noreply.github.com>
2023-10-23 13:30:55 +0530

    Add files via upload

---
+++ 01-Employee-Details-Object/first.js
@@ -0,0 +1,78 @@
+const employees = [
+    {
+      id: "C101",
+      name: "Varun",
+      role: "Junior Web Developer",
+      experience: "2 Years",
+      payout: "25000",
+    },
+    {
+      id: "C102",
+      name: "Srinika",
+      role: "Junior Web Developer",
+      experience: "1 Year",
+      payout: "20000",
+    },
+    {
+      id: "C103",
+      name: "Rakshitha",
+      role: "Front End Developer",
+      experience: "3 Years",
+      payout: "30000",
+    },
+    {
+      id: "C104",
+      name: "Hema",
+      role: "Junior Web Developer",
+      experience: "3 Years",
+      payout: "25000",
+    },
+    {
+      id: "C105",
+      name: "Deepak",
+      role: "Front End Developer",
+      experience: "4 Years",
+      payout: "35000",
+    },
+    {
+      id: "C106",
+      name: "Sakthi",
+      role: "Back End Developer",
+      experience: "2 Years",
+      payout: "30000",
+    },
+    {
+      id: "C107",
+      name: "Dakshan",
+      role: "Senior Front End Developer",
+      experience: "5 Years",
+      payout: "45000",
+    },
+    {
+      id: "C108",
+      name: "Ananth",
+      role: "BDE",
+      experience: "2 Years",
+      payout: "20000",
+    },
+    {
+      id: "C109",
+      name: "Sharaddha",
+      role: "Business Associate",
+      experience: "1 Year",
+      payout: "15000",
+    },
+    {
+      id: "C110",
+      name: "Dinesh",
+      role: "Tech Lead",
+      experience: "4 Years",
+      payout: "65000",
+    },
+  ];
+  
+  const name_employees =employees.map(function(employe){
+    return employe.name;
+  })
+
+  document.write("EMPLOYEES NAME"+" = " +name_employees+" .");
